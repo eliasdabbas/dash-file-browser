87e22191f1b0072ad1ccd860be07fc2af53a8ddb
Elias Dabbas <eliasdabbas@gmail.com>
2022-10-06 00:40:46 +0300

    Minor changes - Sort files & folders with str.lower - Add title tag - Add fork me on GH ribbon - Use folder icon directly - Minor styling changes - Remove unused modified_timestamp property

--- app.py
+++ app.py
@@ -6,36 +6,41 @@ from dash.exceptions import PreventUpdate
 import dash_bootstrap_components as dbc
 
 
-folder_icon = '📂'
-file_idon = '📄'
-
-app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
+app = Dash(__name__, title='Dash File Explorer', external_stylesheets=[dbc.themes.CYBORG])
 server = app.server
 
 app.layout = html.Div([
+    html.Link(
+        rel="stylesheet",
+        href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.3/gh-fork-ribbon.min.css"),
+    html.A(
+        "Fork me on Github",
+        className="github-fork-ribbon",
+        href="https://github.com/eliasdabbas/dash-file-browser",
+        title="Fork me on GitHub", **{"data-ribbon": "Fork me on GitHub"}),
     html.Br(), html.Br(),
     dbc.Row([
-        dbc.Col(lg=1),
+        dbc.Col(lg=1, sm=1, md=1),
         dbc.Col([
             dcc.Store(id='stored_cwd', data=os.getcwd()),
-            html.H1('Dash File Browser'), html.Br(), html.Br(),
+            html.H1('Dash File Browser'),
+            html.Hr(), html.Br(), html.Br(), html.Br(),
             html.H5(html.B(html.A("⬆️ Parent directory", href='#', id='parent_dir'))),
             html.H3([html.Code(os.getcwd(), id='cwd')]),
                     html.Br(), html.Br(),
             html.Div(id='cwd_files'), 
-        ])
+        ], lg=10, sm=11, md=10)
     ])
 ])
 
 
 @app.callback(
     Output("cwd", 'children'),
-    Input('stored_cwd', 'modified_timestamp'),
     Input('stored_cwd', 'data'),
     Input('parent_dir', 'n_clicks'),
     Input("cwd", 'children'),
     prevent_initial_call=True)
-def get_parent_directory(timestamp, stored_cwd, n_clicks, currentdir):
+def get_parent_directory(stored_cwd, n_clicks, currentdir):
     triggered_id = callback_context.triggered_id
     if triggered_id == 'stored_cwd':
         return stored_cwd
@@ -50,17 +55,18 @@ def list_cwd_files(cwd):
     path = Path(cwd)
     cwd_files = []
     if path.is_dir():
-        files = sorted(os.listdir(path))
+        files = sorted(os.listdir(path), key=str.lower)
         for i, file in enumerate(files):
             filepath = Path(file)
-            title=os.path.join(cwd, filepath.as_posix())
-            is_dir = Path(title).is_dir()
-            link = html.A([html.Span(
+            full_path=os.path.join(cwd, filepath.as_posix())
+            is_dir = Path(full_path).is_dir()
+            link = html.A([
+                html.Span(
                 file, id={'type': 'listed_file', 'index': i},
-                title=title,
+                title=full_path,
                 style={'fontWeight': 'bold'} if is_dir else {}
             )], href='#')
-            prepend = '' if not is_dir else folder_icon
+            prepend = '' if not is_dir else '📂'
             cwd_files.append(prepend)
             cwd_files.append(link)
             cwd_files.append(html.Br())
@@ -73,12 +79,11 @@ def list_cwd_files(cwd):
     State({'type': 'listed_file', 'index': ALL}, 'children'),
     State({'type': 'listed_file', 'index': ALL}, 'title'),
     State('cwd', 'children'))
-def display_clicked_file(n_clicks, href, title, cwd):
+def store_clicked_file(n_clicks, href, title, cwd):
     if not n_clicks or set(n_clicks) == {None}:
         raise PreventUpdate
     ctx = callback_context
     index = ctx.triggered_id['index']
-    path = os.path.join(cwd, href[index])
     return title[index]
 
 if __name__ == '__main__':
