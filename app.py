4f56fd9b511dd3b1b32c50b8af6cc85e0f347ea8
Elias Dabbas <eliasdabbas@gmail.com>
2022-11-19 14:07:25 +0300

    Add file meta data size, date created, modified, and icons

--- app.py
+++ app.py
@@ -1,12 +1,64 @@
+import datetime
 import os
 from pathlib import Path
 
-from dash import Dash, callback_context, html, dcc, Input, Output, State, ALL
-from dash.exceptions import PreventUpdate
 import dash_bootstrap_components as dbc
+import pandas as pd
+from dash import ALL, Dash, Input, Output, State, callback_context, dcc, html
+from dash.exceptions import PreventUpdate
+
+from icons import icons
 
 
-app = Dash(__name__, title='Dash File Browser', external_stylesheets=[dbc.themes.CYBORG])
+def icon_file(extension, width=24, height=24):
+    """Retrun an html.img of the svg icon for a given extension."""
+    filetype = icons.get(extension)
+    file_name = f'file_type_{filetype}.svg' if filetype is not None else 'default_file.svg'
+    html_tag = html.Img(src=app.get_asset_url(f'icons/{file_name}'),
+                        width=width, height=height)
+    return html_tag
+
+
+def nowtimestamp(timestamp, fmt='%b %d, %Y %H:%M'):
+    return datetime.datetime.fromtimestamp(timestamp).strftime(fmt)
+
+
+def file_info(path):
+    """Get file info for a given path.
+
+    Uncomment the attributes that you want to display.
+
+    Parameters:
+    -----------
+    path : pathlib.Path object
+    """
+    file_stat = path.stat()
+    d = {
+        'extension': path.suffix if not path.name.startswith('.') else path.name,
+        'filename': path.name,
+        # 'fullpath': str(path.absolute()),
+        'size': format(file_stat.st_size, ','),
+        'created': nowtimestamp(file_stat.st_ctime),
+        'modified': nowtimestamp(file_stat.st_mtime),
+        # 'accessed': nowtimestamp(file_stat.st_atime),
+        # 'is_dir': str(path.is_dir()),
+        # 'is_file': str(path.is_file()),
+    }
+    return d
+
+
+app = Dash(
+    __name__,
+    title='Dash File Browser',
+    assets_folder='assets',
+    meta_tags=[
+        {'name': 'description',
+         'content': """Dash File Browser - a simple browser for files that is \
+used to explore files and folder on the server. This is useful if your users \
+need to manipulate and analyze files on the server."""},
+        ],
+    external_stylesheets=[dbc.themes.FLATLY])
+
 server = app.server
 
 app.layout = html.Div([
@@ -25,20 +77,22 @@ app.layout = html.Div([
             dcc.Store(id='stored_cwd', data=os.getcwd()),
             html.H1('Dash File Browser'),
             html.Hr(), html.Br(), html.Br(), html.Br(),
-            html.H5(html.B(html.A("⬆️ Parent directory", href='#', id='parent_dir'))),
+            html.H5(html.B(html.A("⬆️ Parent directory", href='#',
+                                  id='parent_dir'))),
             html.H3([html.Code(os.getcwd(), id='cwd')]),
                     html.Br(), html.Br(),
-            html.Div(id='cwd_files'), 
+            html.Div(id='cwd_files',
+                     style={'height': 500, 'overflow': 'scroll'}),
         ], lg=10, sm=11, md=10)
     ])
-])
+] + [html.Br() for _ in range(15)])
 
 
 @app.callback(
-    Output("cwd", 'children'),
+    Output('cwd', 'children'),
     Input('stored_cwd', 'data'),
     Input('parent_dir', 'n_clicks'),
-    Input("cwd", 'children'),
+    Input('cwd', 'children'),
     prevent_initial_call=True)
 def get_parent_directory(stored_cwd, n_clicks, currentdir):
     triggered_id = callback_context.triggered_id
@@ -53,7 +107,7 @@ def get_parent_directory(stored_cwd, n_clicks, currentdir):
     Input('cwd', 'children'))
 def list_cwd_files(cwd):
     path = Path(cwd)
-    cwd_files = []
+    all_file_details = []
     if path.is_dir():
         files = sorted(os.listdir(path), key=str.lower)
         for i, file in enumerate(files):
@@ -64,22 +118,30 @@ def list_cwd_files(cwd):
                 html.Span(
                 file, id={'type': 'listed_file', 'index': i},
                 title=full_path,
-                style={'fontWeight': 'bold'} if is_dir else {}
+                style={'fontWeight': 'bold', 'fontSize': 18} if is_dir else {}
             )], href='#')
-            prepend = '' if not is_dir else '📂'
-            cwd_files.append(prepend)
-            cwd_files.append(link)
-            cwd_files.append(html.Br())
-    return cwd_files
+            details = file_info(Path(full_path))
+            details['filename'] = link
+            if is_dir:
+                details['extension'] = html.Img(
+                    src=app.get_asset_url('icons/default_folder.svg'),
+                    width=25, height=25)
+            else:
+                details['extension'] = icon_file(details['extension'][1:])
+            all_file_details.append(details)
+
+    df = pd.DataFrame(all_file_details)
+    df = df.rename(columns={"extension": ''})
+    table = dbc.Table.from_dataframe(df, striped=False, bordered=False,
+                                     hover=True, size='sm')
+    return html.Div(table)
 
 
 @app.callback(
     Output('stored_cwd', 'data'),
     Input({'type': 'listed_file', 'index': ALL}, 'n_clicks'),
-    State({'type': 'listed_file', 'index': ALL}, 'children'),
-    State({'type': 'listed_file', 'index': ALL}, 'title'),
-    State('cwd', 'children'))
-def store_clicked_file(n_clicks, href, title, cwd):
+    State({'type': 'listed_file', 'index': ALL}, 'title'))
+def store_clicked_file(n_clicks, title):
     if not n_clicks or set(n_clicks) == {None}:
         raise PreventUpdate
     ctx = callback_context
